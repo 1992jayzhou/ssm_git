0f5fa2d54aa306f9eaa08884a5654c48f8b91d55
liy <ly@ly.com>
2019-10-17 18:58:02 +0800

    dev1修改 this project

---
+++ src/main/java/com/itheima/domain/Dev01.java
@@ -0,0 +1,10 @@
+package com.itheima.domain;
+
+import java.io.Serializable;
+
+public class Dev01 implements Serializable {
+
+    public void test1(){
+        System.out.println("我是dev1");
+    }
+}
